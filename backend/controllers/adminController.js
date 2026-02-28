0c9fd8f5a46086b6a54e61068a132974287c21ba
rohit biswas <rbrohitbiswas07@gmail.com>
2026-03-01 02:58:09 +0530

    fix: restore User model import in adminController

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const Admin = require('../models/Admin');
+const User = require('../models/User');
 const Subscriber = require('../models/Subscriber');
 
 const adminLogin = async (req, res) => {
